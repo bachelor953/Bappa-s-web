7280747d7261152a0dc8d242bddd006ad1973d9e
bappa4321 <Bappa121235@gmail.com>
2026-01-05 23:11:33 +0530

    update

--- a/socket/socket.js
+++ a/socket/socket.js
@@ -1,13 +1,41 @@
 const { Server } = require("socket.io");
 
-module.exports = (server)=>{
-  const io = new Server(server,{
-    cors:{ origin:"*" }
+module.exports = function (server) {
+  const io = new Server(server, {
+    cors: { origin: "*" }
   });
 
-  io.on("connection", socket=>{
-    socket.on("sendMsg", data=>{
-      io.emit("getMsg", data);
+  // userId -> socketId
+  const onlineUsers = {};
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    // user online register
+    socket.on("addUser", (userId) => {
+      onlineUsers[userId] = socket.id;
+      console.log("Online users:", onlineUsers);
+    });
+
+    // private message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+      const receiverSocket = onlineUsers[receiverId];
+
+      if (receiverSocket) {
+        io.to(receiverSocket).emit("getMessage", {
+          senderId,
+          text
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      for (const userId in onlineUsers) {
+        if (onlineUsers[userId] === socket.id) {
+          delete onlineUsers[userId];
+        }
+      }
+      console.log("User disconnected");
     });
   });
 };
