2e7c0e280b006604343123248ca7347ae061dc14
bappa4321 <Bappa121235@gmail.com>
2026-01-07 20:50:29 +0530

    update all

--- a/socket/socket.js
+++ a/socket/socket.js
@@ -1,14 +1,14 @@
 const { Server } = require("socket.io");
 const Message = require("../models/Message");
 
-module.exports = function (server) {
+module.exports = function(server){
   const io = new Server(server,{ cors:{origin:"*"} });
   const onlineUsers = {};
 
-  io.on("connection", socket => {
+  io.on("connection",socket=>{
 
-    socket.on("addUser", userId=>{
-      onlineUsers[userId]=socket.id;
+    socket.on("addUser",id=>{
+      onlineUsers[id]=socket.id;
       io.emit("onlineUsers",Object.keys(onlineUsers));
     });
 
@@ -16,69 +16,71 @@ module.exports = function (server) {
       socket.emit("onlineUsers",Object.keys(onlineUsers));
     });
 
-    socket.on("typing",({senderName,receiverId})=>{
-      const s=onlineUsers[receiverId];
-      if(s) io.to(s).emit("typing",{senderName});
+    socket.on("typing",d=>{
+      const s=onlineUsers[d.receiverId];
+      if(s) io.to(s).emit("typing",{senderName:d.senderName});
     });
 
-    socket.on("stopTyping",({receiverId})=>{
-      const s=onlineUsers[receiverId];
+    socket.on("stopTyping",d=>{
+      const s=onlineUsers[d.receiverId];
       if(s) io.to(s).emit("stopTyping");
     });
 
-    socket.on("callUser",({callerId,callerName,receiverId})=>{
-      const s=onlineUsers[receiverId];
-      if(s) io.to(s).emit("incomingCall",{callerId,callerName});
+    socket.on("sendMessage",async d=>{
+      const s=onlineUsers[d.receiverId];
+      await new Message({
+        senderId:d.senderId,
+        receiverId:d.receiverId,
+        text:d.text,
+        status:s?"delivered":"sent"
+      }).save();
+
+      if(s){
+        io.to(s).emit("getMessage",{
+          senderName:d.senderName,
+          text:d.text
+        });
+      }
     });
 
-    socket.on("acceptCall",({callerId})=>{
-      const s=onlineUsers[callerId];
+    socket.on("callUser",d=>{
+      const s=onlineUsers[d.receiverId];
+      if(s) io.to(s).emit("incomingCall",d);
+    });
+
+    socket.on("acceptCall",d=>{
+      const s=onlineUsers[d.callerId];
       if(s) io.to(s).emit("callAccepted");
     });
 
-    socket.on("rejectCall",({callerId})=>{
-      const s=onlineUsers[callerId];
+    socket.on("rejectCall",d=>{
+      const s=onlineUsers[d.callerId];
       if(s) io.to(s).emit("callRejected");
     });
 
-    socket.on("endCall",({otherUserId})=>{
-      const s=onlineUsers[otherUserId];
+    socket.on("endCall",d=>{
+      const s=onlineUsers[d.otherUserId];
       if(s) io.to(s).emit("callEnded");
     });
 
-    socket.on("webrtc-offer",({to,offer})=>{
-      const s=onlineUsers[to];
-      if(s) io.to(s).emit("webrtc-offer",offer);
+    socket.on("webrtc-offer",d=>{
+      const s=onlineUsers[d.to];
+      if(s) io.to(s).emit("webrtc-offer",d.offer);
     });
 
-    socket.on("webrtc-answer",({to,answer})=>{
-      const s=onlineUsers[to];
-      if(s) io.to(s).emit("webrtc-answer",answer);
+    socket.on("webrtc-answer",d=>{
+      const s=onlineUsers[d.to];
+      if(s) io.to(s).emit("webrtc-answer",d.answer);
     });
 
-    socket.on("webrtc-ice",({to,candidate})=>{
-      const s=onlineUsers[to];
-      if(s) io.to(s).emit("webrtc-ice",candidate);
-    });
-
-    socket.on("sendMessage",async d=>{
-      const msg=new Message({
-        senderId:d.senderId,
-        receiverId:d.receiverId,
-        text:d.text
-      });
-      await msg.save();
-
-      const s=onlineUsers[d.receiverId];
-      if(s) io.to(s).emit("getMessage",{
-        senderName:d.senderName,
-        text:d.text
-      });
+    socket.on("webrtc-ice",d=>{
+      const s=onlineUsers[d.to];
+      if(s) io.to(s).emit("webrtc-ice",d.candidate);
     });
 
     socket.on("disconnect",()=>{
-      for(const u in onlineUsers)
-        if(onlineUsers[u]===socket.id) delete onlineUsers[u];
+      for(let k in onlineUsers)
+        if(onlineUsers[k]===socket.id) delete onlineUsers[k];
       io.emit("onlineUsers",Object.keys(onlineUsers));
     });
   });
